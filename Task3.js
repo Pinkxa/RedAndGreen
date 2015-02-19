c7ba8a3cd8d498a4dd000fc9f9dd7ce39feb5e98
Pretty <pretty@projectcodex.co>
2015-02-19 16:41:36 +0200

    Task3.js is done

---
+++ Task3.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("first").style.backgroundColor = "Green";
+ }
+
+ function myFunction1() {
+    document.getElementById("second").style.backgroundColor = "Red";
+ }

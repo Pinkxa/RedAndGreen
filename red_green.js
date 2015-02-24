1a14d90f9b1983f40e61cf527775e71752084310
Pretty <pretty@projectcodex.co>
2015-02-24 14:44:32 +0200

    redorgreen is done

---
+++ red_green.js
@@ -0,0 +1,8 @@
+function RedOrGreen(id) {
+this.makeGreen = function() {
+	document.getElementById(id).style.backgroundColor = "Green";
+}
+this.makeRed = function() {
+	document.getElementById(id).style.backgroundColor = "Red";
+}
+}
---
+++ red_green_test.js
@@ -0,0 +1,9 @@
+ var rg = new RedOrGreen("myElement");
+
+   rg.makeGreen();
+   // myElements background color should be green now
+
+
+   rg.makeRed();
+   // myElements background should be red now
+
